8969fbb584044eef471033d26dae51b6dc643550
Finanda Wafiq <finnandawafiq@gmail.com>
2022-10-17 23:14:06 +0700

    First Commit

---
+++ src/main/java/FirstMeet_PWS/Penjualan_Barangg/PenjualanBaranggApplication.java
@@ -0,0 +1,13 @@
+package FirstMeet_PWS.Penjualan_Barangg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenjualanBaranggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenjualanBaranggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FirstMeet_PWS/Penjualan_Barangg/PenjualanBaranggApplicationTests.java
@@ -0,0 +1,13 @@
+package FirstMeet_PWS.Penjualan_Barangg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenjualanBaranggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
